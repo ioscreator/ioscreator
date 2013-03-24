125a34a40d73d3d78a5b7eee7b33fa62f5f6e56c
Arthur Knopper <acknopper@gmail.com>
2013-03-24 21:44:37 +0100

    added EmailDemo

---
+++ EmailDemo/EmailDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  EmailDemo
+//
+//  Created by Arthur Knopper on 7/10/12.
+//  Copyright (c) 2012 iOSCreator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
