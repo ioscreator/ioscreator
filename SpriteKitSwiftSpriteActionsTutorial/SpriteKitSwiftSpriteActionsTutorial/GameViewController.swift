623572d4fa65f1a20154fee13624a56600fd29a4
iOScreator <dev@ioscreator.com>
2014-11-08 09:44:07 +0100

    updated for Swift 1.1

--- SpriteKitSwiftSpriteActionsTutorial/SpriteKitSwiftSpriteActionsTutorial/GameViewController.swift
+++ SpriteKitSwiftSpriteActionsTutorial/SpriteKitSwiftSpriteActionsTutorial/GameViewController.swift
@@ -10,18 +10,19 @@ import UIKit
 import SpriteKit
 
 extension SKNode {
-    class func unarchiveFromFile(file : NSString) -> SKNode? {
-        
-        let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks")
-        
-        var sceneData = NSData.dataWithContentsOfFile(path, options: .DataReadingMappedIfSafe, error: nil)
-        var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-        
-        archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-        let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
-        archiver.finishDecoding()
-        return scene
+  class func unarchiveFromFile(file : NSString) -> SKNode? {
+    if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+      var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+      var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+      
+      archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+      let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
+      archiver.finishDecoding()
+      return scene
+    } else {
+      return nil
     }
+  }
 }
 
 class GameViewController: UIViewController {
@@ -49,13 +50,14 @@ class GameViewController: UIViewController {
         return true
     }
 
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
-        } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
-        }
+  override func supportedInterfaceOrientations() -> Int {
+    if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+      return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+    } else {
+      return Int(UIInterfaceOrientationMask.All.rawValue)
     }
+  }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
