50675863a410f808f5c0f59f845ade98864edc5c
ioscreator <dev@ioscreator.com>
2019-07-15 19:52:31 +0200

    SwiftUISliderTutorial added

---
+++ SwiftUISliderTutorial/SwiftUISliderTutorial/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  SwiftUISliderTutorial
+//
+//  Created by Arthur Knopper on 15/07/2019.
+//  Copyright © 2019 Arthur Knopper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    @State var sliderValue = 0.0
+    var minimumValue = 0.0
+    var maximumvalue = 100.0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("\(Int(minimumValue))")
+                Slider(value: $sliderValue, from: minimumValue, through: maximumvalue, by: 10)
+                Text("\(Int(maximumvalue))")
+            }.padding()
+        
+            Text("\(Int(sliderValue))")
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
